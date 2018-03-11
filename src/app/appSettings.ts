f3b1e3d862f230497d68effaf6bddb4603734641
Yuliana Apaza <yuliana.apaza94@gmail.com>
2018-03-10 21:58:36 -0500

    Use HTTPS for backend server

--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static API_ENDPOINT = 'http://aqueous-mountain-38515.herokuapp.com';
+    public static API_ENDPOINT = 'https://aqueous-mountain-38515.herokuapp.com';
     //public static API_ENDPOINT = 'http://localhost:3000';
     public static API_ARAPP = AppSettings.API_ENDPOINT + "/arapp";
     public static POLY_API_KEY = 'AIzaSyCK4hsae4rnz5ttw5pK6-v0vXipU6D63as';
